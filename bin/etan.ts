c29160cb724f35525c61e487ce01e13a3040a650
huangqing <2316694914@qq.com>
2021-06-22 17:51:33 +0800

    refactor: command loader

--- bin/etan.ts
+++ bin/etan.ts
@@ -1,17 +1,29 @@
-const tsConfigFileName = 'test-cli.json';
+#!/usr/bin/env node
 
-import { Start } from '../lib/actions/index';
+import * as commander from 'commander';
+import { CommanderStatic } from 'commander';
+// const tsConfigFileName = 'test-cli.json';
+// import { StartAction } from '../lib/actions/index';
 
-const bootstrap = () => {
-  const start = new Start();
-  const options = {
-    watch: false,
-    debug: false,
-    preserveWatchOutput: false,
-    config: tsConfigFileName,
-  };
-  
-  start.run(options);
+import { CommandLoader } from '../lib/commands';
+
+function bootstrap(){
+  const program: CommanderStatic = commander;
+  program
+    .version(
+      require('../package.json').version,
+      '-v, --version',
+      '(v) Outputs Etan CLI version.'
+    )
+    .usage('<command> [options]')
+    .helpOption('-h ,--help','(h) Lists available commands and their short descriptions.');
+
+  CommandLoader.load(program);
+
+  program.parse(process.argv);
+  if (!process.argv.slice(2).length) {
+    program.outputHelp();
+  }
 };
 
 bootstrap();
