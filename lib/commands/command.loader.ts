65b13b932a67cbe1fd4d48a8c29ea3f8c2d8a1ae
huangqing <2316694914@qq.com>
2021-06-22 17:54:26 +0800

    feat: invalidCommand

--- lib/commands/command.loader.ts
+++ lib/commands/command.loader.ts
@@ -1,4 +1,4 @@
-import { Command, CommanderStatic } from 'commander';
+import { CommanderStatic } from 'commander';
 import { StartAction } from '../actions';
 import { StartCommand } from './start.command';
 
@@ -6,5 +6,13 @@ export class CommandLoader {
   public static load(program: CommanderStatic) {
     const startAction = new StartAction();
     new StartCommand(startAction).load(program);
+    this.invalidCommand(program);
+  }
+  private static invalidCommand(program: CommanderStatic) {
+    program.on('command:*', () => {
+      console.error(`\nERROR Invalid command: `, program.args.join(' '));
+      console.log(`See --help for a list of available commands.\n`);
+      process.exit(1);
+    });
   }
 }
