34642efbdb111a50d4e02de6701bb6d9ba18c521
huangqing <2316694914@qq.com>
2021-06-25 16:21:03 +0800

    chore: change file name

--- lib/actions/start.action.ts
+++ lib/actions/start.action.ts
@@ -1,6 +1,6 @@
 //http://nodejs.cn/api/child_process.html#child_process_child_process_spawn_command_args_options
 import { spawn } from 'child_process';
-import { BuildAction } from './build';
+import { BuildAction } from './build.action';
 
 export class StartAction extends BuildAction {
   protected spawnChildProcess(debug?: boolean | string) {
