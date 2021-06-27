a6c33699cb8d0e17467509b2a168d8b817f3ad14
huangqing <2316694914@qq.com>
2021-06-27 17:25:02 +0800

    feat: info alias 'i'

--- lib/commands/info.command.ts
+++ lib/commands/info.command.ts
@@ -5,6 +5,7 @@ export class InfoCommand extends AbstractCommand {
   public load(program: CommanderStatic) {
     program
       .command('info')
+      .alias('i')
       .description('Display Etan project details.')
       .action(async () => {
         await this.action.handle();
