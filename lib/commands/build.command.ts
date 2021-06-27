9169ba7e29a80964562362e2d238988cd8cd8d25
huangqing <2316694914@qq.com>
2021-06-27 11:37:02 +0800

    feat: build command

--- lib/commands/build.command.ts
+++ lib/commands/build.command.ts
@@ -1,3 +1,49 @@
-import { Command } from 'commander';
-
-export class BuildCommand {}
+import { CommanderStatic } from 'commander';
+import { AbstractCommand } from './abstract.command';
+import { Options } from './constant';
+//https://github.com/tj/commander.js/blob/HEAD/Readme_zh-CN.md
+export class BuildCommand extends AbstractCommand {
+  public load(program: CommanderStatic) {
+    program
+      // .command('build')
+      .command('build')
+      .usage('<command> [options]')
+      .option(
+        '-m, --mac [targetList...]',
+        'Build for macOS, accepts target list (see https://goo.gl/5uHuzj)',
+      )
+      .option(
+        '-l, --linux [targetList...]',
+        'Build for Linux, accepts target list (see https://goo.gl/',
+      )
+      .option(
+        '-w, --windows [targetList...]',
+        'Build for Windows, accepts target list (see https://goo.gl/jYsTEJ)',
+      )
+      .option('--x64', 'Build for x64')
+      .option('--ia32', 'Build for ia32')
+      .option('--armv7l', 'Build for armv7l')
+      .option('--arm64', 'Build for arm64')
+      .option('--dir', 'Build unpacked dir. Useful to test.')
+      .option(
+        '--pd, --prepackaged',
+        'The path to prepackaged app (to pack in a distributable format)',
+      )
+      .option(
+        '--projectDir, --project [path]',
+        'The path to project directory. Defaults to current',
+      )
+      .option(
+        '-c, --config [path]',
+        "Defaults to 'electron-builder.yml' (or 'json', or 'json5'), see https://goo.gl/YFRJOM",
+      )
+      .option(
+        '-p, --publish [options...]',
+        'Publish artifacts (to GitHub Releases), see https://goo.gl/tSFycD [choices: "onTag", "onTagOrDraft", "always",  "never", undefined]',
+      )
+      .description('Output electron-builder cli build command info')
+      .action(async (command) => {
+        await this.action.handle(command);
+      });
+  }
+}
--- lib/commands/command.loader.ts
+++ lib/commands/command.loader.ts
@@ -1,14 +1,17 @@
 import { CommanderStatic } from 'commander';
-import { StartAction, InfoAction } from '../actions';
+import { StartAction, InfoAction, BuildAction } from '../actions';
 import { InfoCommand } from './info.command';
 import { StartCommand } from './start.command';
+import { BuildCommand } from './build.command';
 
 export class CommandLoader {
   public static load(program: CommanderStatic) {
     const startAction = new StartAction();
     const infoAction = new InfoAction();
+    const buildAction = new BuildAction();
     new StartCommand(startAction).load(program);
     new InfoCommand(infoAction).load(program);
+    new BuildCommand(buildAction).load(program);
     this.invalidCommand(program);
   }
   private static invalidCommand(program: CommanderStatic) {
